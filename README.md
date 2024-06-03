b0733be6bb8ecb036e3877bd0e286e489db263b1
praveen2002j <jeyachandranpraveen2002@gmail.com>
2024-06-03 17:32:32 +0530

    final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mobile-Memory-game
+This repository contains an event-driven memory game project made with XML and Kotlin in Android Studio. Users are able to interact with the application by selecting and matching the corresponding cards. Once matched, the opacity of the card is decreased, and users can no longer select it as an option.
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MemGame1"
+include(":app")
+ 
